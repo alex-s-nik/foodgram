3677b9e083c44c6b68f0450d0b4830228449b447
alex-s-nik <to_ctrl@mail.ru>
2022-11-28 20:11:31 +0300

    feat: add fields in user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,14 +1,33 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from recipes.models import Recipe
 
 
 class User(AbstractUser):
     '''Пользователь системы'''
-    favorites = ...
-    shopping_cart = ...
+    favorites = models.ManyToManyField(
+        to=Recipe,
+        related_name='users',
+        verbose_name='Избранное'
+    )
+    shopping_cart = models.ManyToManyField(
+        to=Recipe,
+        related_name='users',
+        verbose_name='Список покупок'
+    )
 
 
 class Follow(models.Model):
     '''Подписка на пользователя'''
-    user = ...
-    author = ...
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='author',
+        verbose_name='Автор'
+    )
