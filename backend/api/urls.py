f915e2487cba7080b0a55a003f278ca8bafc09da
alex-s-nik <to_ctrl@mail.ru>
2022-12-01 16:29:17 +0300

    feat: add users urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
-
+from api.views import UserViewSet
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
+    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('users/<int:id>/', UserViewSet.as_view({'get': 'retrieve'})),
+    path('users/me/', UserViewSet.as_view({'get': 'me'})),
+    path('users/set_password/', UserViewSet.as_view({'post': 'set_password'}))
 ]
