e7c805b5fc745b43616f180ff63ee49ec1b8b234
alex-s-nik <to_ctrl@mail.ru>
2022-12-15 21:16:51 +0300

    feat: add filters and fields in recipe admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from .models import AmountIngridients, Ingridient, Recipe, Tag
 
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'count_field',)
+    list_filter = ('author', 'name', 'tags',)
+
+    @admin.display(description='Добавлены в избранное')
+    def count_field(self, obj):
+        return obj.favorite_users.count()
+
+
 admin.site.register(AmountIngridients)
 admin.site.register(Ingridient)
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Tag)
