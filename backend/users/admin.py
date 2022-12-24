160017d1c5c80ca390f83e3f01eb1fdedd1209e9
alex-s-nik <to_ctrl@mail.ru>
2022-12-24 12:38:18 +0300

    fix: hide Groups tab in admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from .models import User
 
@@ -9,3 +10,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(Group)
