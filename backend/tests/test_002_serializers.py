8b0da21c74da965c6b9c0ba05141c17ed1b1a7a9
alex-s-nik <to_ctrl@mail.ru>
2024-01-11 23:35:44 +0300

    feat: add test tag and ingredient serializers

--- backend/tests/test_002_serializers.py
+++ backend/tests/test_002_serializers.py
@@ -1,3 +1,9 @@
+import pytest
+
+from api.serializers import IngredientSerializer, TagSerializer
+from recipes.factories import IngredientFactory, TagFactory
+
+
 class TestSerializers:
     """Тестирование сериалайзеров."""
 
@@ -9,13 +15,37 @@ class TestSerializers:
         """Сериалайзер создания Пользователя."""
         ...
 
+    @pytest.mark.django_db()
     def test_tag_serializer(self):
         """Сериалайзер Тэга."""
-        ...
+        tag = TagFactory.build()
+        tag_data = {'name': tag.name, 'color': tag.color, 'slug': tag.slug}
 
+        tag_serializer = TagSerializer(tag)
+        tag_serializer_data = tag_serializer.data
+        del tag_serializer_data['id']
+
+        assert tag_data == tag_serializer_data
+
+        tag_serializer_from_data = TagSerializer(data=tag_data)
+        assert tag_serializer_from_data.is_valid()
+        assert tag_serializer_from_data.validated_data == tag_data
+
+    @pytest.mark.django_db()
     def test_ingredient_serializer(self):
         """Сериалайзер Ингридиента."""
-        ...
+        ingredient = IngredientFactory.build()
+        ingredient_data = {'name': ingredient.name, 'measurement_unit': ingredient.measurement_unit}
+
+        ingredient_serializer = IngredientSerializer(ingredient)
+        ingredient_serializer_data = ingredient_serializer.data
+        del ingredient_serializer_data['id']
+
+        assert ingredient_data == ingredient_serializer_data
+
+        ingredient_serializer_from_data = IngredientSerializer(data=ingredient_data)
+        assert ingredient_serializer_from_data.is_valid()
+        assert ingredient_serializer_from_data.validated_data == ingredient_data
 
     def test_amount_ingredient_serializer(self):
         """Сериалайзер Количества ингридиента."""
